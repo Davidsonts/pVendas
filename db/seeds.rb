72d27153a7acc8177cff41eda786e9d9f4605e9e
Davidson <davidsonts@outlook.com>
2020-09-11 10:08:33 -0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create name: 'David', status: :active, kind: :manager, email: 'manager@gmail.com', password: 123456
+User.create name: 'Alan', status: :active, kind: :salesman, email: 'alan@gmail.com', password: 123456
+User.create name: 'Maria', status: :active, kind: :salesman, email: 'maria@gmail.com', password: 123456
+
+Client.create name: 'Jõao', status: :active
+Client.create name: 'Mario', status: :active
+Client.create name: 'Loja  Ltda', status: :active
+Client.create name: 'Loja Dois', status: :active
+
+Product.create name: 'MacBook Pro', price: 10000, user_id: 1
+Product.create name: 'Ipad', price: 5000, user_id: 1
+Product.create name: 'Iphone 7 Plus', price: 3000, user_id: 1
+Product.create name: 'MacBook Air', price: 9000, user_id: 1
