ce1f99719d7fe572cb7fbddf8825e7904c35f1ff
Davidson <davidsonts@outlook.com>
2020-09-10 23:27:52 -0300

    add new migrates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :user
+  enum status: [:active, :inactive]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :products
+  enum kind: [:salesman, :manager]
+  enum status: [:active, :inactive]
 end
