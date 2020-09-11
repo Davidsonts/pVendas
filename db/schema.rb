d8edddb223fbad7c6e60ebd5b70827eff515a145
Davidson <davidsonts@outlook.com>
2020-09-10 23:47:19 -0300

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_11_022758) do
+ActiveRecord::Schema.define(version: 2020_09_11_024614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 2020_09_11_022758) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "kind"
     t.string "status"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "product_sales", "products"
